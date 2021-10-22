a3ec370e7c2579b35e8a32e557ade5f92a65312c
93-silvestre <nathyribsilvestre@gmail.com>
2021-10-22 01:08:56 -0300

    Projeto YouGroup/Lion Code

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { QuemSomosComponent } from './quem-somos/quem-somos.component';
+import { MenuComponent } from './menu/menu.component';
+import { SolucoesComponent } from './solucoes/solucoes.component';
+import { PortifolioComponent } from './portifolio/portifolio.component';
+import { CreatorsComponent } from './creators/creators.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { ContatoComponent } from './contato/contato.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuemSomosComponent,
+    MenuComponent,
+    SolucoesComponent,
+    PortifolioComponent,
+    CreatorsComponent,
+    ClientesComponent,
+    ContatoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
