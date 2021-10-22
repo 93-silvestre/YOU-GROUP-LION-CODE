615eee0873b18852630eb9d312f4298ed9cb46f0
93-silvestre <nathyribsilvestre@gmail.com>
2021-10-22 02:05:47 -0300

    Implementando Rotas de acesso

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientesComponent } from './clientes/clientes.component';
+import { ContatoComponent } from './contato/contato.component';
+import { CreatorsComponent } from './creators/creators.component';
+import { PortifolioComponent } from './portifolio/portifolio.component';
+import { QuemSomosComponent } from './quem-somos/quem-somos.component';
+import { SolucoesComponent } from './solucoes/solucoes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', redirectTo: 'quem-somos', pathMatch: 'full'},
+
+  {path:'quem-somos', component: QuemSomosComponent},
+  {path:'solucoes', component: SolucoesComponent},
+  {path:'portifolio', component: PortifolioComponent},
+  {path:'creators', component: CreatorsComponent},
+  {path:'clientes', component: ClientesComponent},
+  {path:'contato', component: ContatoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
